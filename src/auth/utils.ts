9827ccda7d6de2891d67c1e6d2012faf95930151
isdance <isdance2004@hotmail.com>
2020-06-08 16:03:17 +1000

    add auth

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -2,6 +2,8 @@ import { decode } from 'jsonwebtoken'
 
 import { JwtPayload } from './JwtPayload'
 
+import Axios from 'axios'
+
 /**
  * Parse a JWT token and return a user id
  * @param jwtToken JWT token to parse
@@ -10,4 +12,22 @@ import { JwtPayload } from './JwtPayload'
 export function parseUserId(jwtToken: string): string {
   const decodedJwt = decode(jwtToken) as JwtPayload
   return decodedJwt.sub
+}
+
+/**
+ * Parse a JWT token and return a user id
+ * @param jwksUrl welknown jwksUrl
+ * @returns a rs256 certificate
+ */
+export const getCertificate = async (jwksUrl: string): Promise<string> => {
+  const jwks = await Axios.get(jwksUrl)
+
+  if (!jwks || !jwks.data) throw new Error('Invalid JSON Web Key Set')
+
+  const { keys } = jwks.data
+
+  if (!Array.isArray(keys) || !keys.length)
+    throw new Error('Invalid JSON Web Key Set')
+
+  return `-----BEGIN CERTIFICATE-----\n${keys[0].x5c[0]}\n-----END CERTIFICATE-----`
 }
